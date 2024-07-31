0170691e06c06c385cdfc763b01fbd06106fa6ab
Nico Forteza <nicoforteza@gmail.com>
2024-07-31 18:12:34 +0200

    update for replication. within remains

--- analysis_diff_between.R
+++ analysis_diff_between.R
@@ -1,4 +1,4 @@
-setwd("/Users/nicoforteza/Desktop/tfm")
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 library(tidyverse)
 library(fixest)
 library(modelsummary)
@@ -7,12 +7,11 @@ library(glue)
 library(countrycode)
 library(kableExtra)
 library(hdm)
-require(WeightIt)
 library(did2s)
-library(ggiplot)
+library(ggfixest)
+
 library(showtext)
 source("config.R")
-font_add("Palatino", "C:/Windows/Fonts/pala.ttf")
 
 showtext_auto() 
 
@@ -31,12 +30,6 @@ df = right_join(df, tibble(country_id=codes, country=paises),
                 by="country_id") %>% 
   mutate(country=as.factor(country))
 
-wm <- weightit(treat ~ log_pop_ras + log_exp_w + country, 
-               data = df, 
-               stabilize = T)
-df = df %>% 
-  mutate(w=as.vector(wm$weights))
-
 treated_countries = df %>% filter(treat == 1) %>% select(country_id) %>% unique() %>% as.vector()
 treated_ids = df %>% filter(treat == 1) %>% select(id) %>% unique() %>% as.vector()
 
@@ -45,14 +38,14 @@ df_ids = df %>% filter(id %in% treated_ids$id)
 
 # Static DiD  #####
 
-iv1 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_pop_ras ~ ..hist, df_countries)
-iv2 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_dens_exp ~ ..hist, df_countries)
+iv1 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_pop_ras ~ ..hist_between, df_countries)
+iv2 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_dens_exp ~ ..hist_between, df_countries)
 
 etable(
   iv1, iv2,
-  fitstat=c('n', 'ar2', 'ivf1'),
+  fitstat=c('n', 'ar2', 'ivf1', "wh", "sargan"),
   digits=3,
-  title='Treatment Effect of Urban Access Regulation on PM2.5 Exposure',
+  title='Treatment Effect of Low Emission Zone on PM2.5 Exposure',
   depvar=F,
   se.below = T,
   drop=c("mean_temp", "mean_prec", "mean_wind", "lat", 
@@ -67,16 +60,19 @@ etable(
   ),
   tex = T,
   digits.stats=3,
-  style.df = style.df(fixef.title = "", fixef.suffix = " FE", yesNo = c("Yes", 'No'))
+  label='tab:did_all',
+  file = "tables/v2/static_did_all.tex",
+  notes="Notes: Signif. Codes: ***: 0.01, **: 0.05, *: 0.1. Clustered (country-year) standard-errors in parentheses. Fixed Effects: Country-year, country and year. Temporal coverage: 2007-2021. Countries: Albania, Austria, Belgium, Bulgaria, Croatia, Cyprus, Czechia, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Italy, Latvia, Lithuania, Montenegro, Netherlands, North Macedonia, Norway, Poland, Portugal, Romania, Serbia, Slovakia, Slovenia, Spain, Sweden, Switzerland and United Kingdom.",
+  replace = T
 )
 
-iv1 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_pop_ras ~ ..hist, df_countries, split = ~ country)
+iv1 = feols(log_exp_w ~ ..c2 + treat | ..fe | log_pop_ras ~ ..hist_between, df_countries, split = ~ country)
 
 etable(
   iv1,
-  fitstat=c('n', 'ar2', 'ivf1'),
+  fitstat=c('n', 'ar2', 'ivf1', "wh", "sargan"),
   digits=3,
-  title='Treatment Effect of Urban Access Regulation on PM2.5 Exposure',
+  title='Treatment Effect of Low Emission Zone on PM2.5 Exposure',
   depvar=F,
   se.below = T,
   drop=c("mean_temp", "mean_prec", "mean_wind", "lat", 
@@ -91,7 +87,10 @@ etable(
   ),
   tex = T,
   digits.stats=3,
-  style.df = style.df(fixef.title = "", fixef.suffix = " FE", yesNo = c("Yes", 'No'))
+  label='tab:did_country',
+  file = "tables/v2/static_did_country.tex",
+  notes="Notes: Signif. Codes: ***: 0.01, **: 0.05, *: 0.1. Clustered (country-year) standard-errors in parentheses. Fixed Effects: Country-year, country and year. Temporal coverage: 2007-2021. Countries: Albania, Austria, Belgium, Bulgaria, Croatia, Cyprus, Czechia, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Italy, Latvia, Lithuania, Montenegro, Netherlands, North Macedonia, Norway, Poland, Portugal, Romania, Serbia, Slovakia, Slovenia, Spain, Sweden, Switzerland and United Kingdom.",
+  replace = T
 )
 
 # Event Time Study ####
@@ -100,72 +99,38 @@ etable(
 dfm = df_countries %>% 
   mutate(treat = replace(treat, id %in% treated_ids$id %>% as.vector(), 1))
 
-est_twfe = feols(
-  log_exp_w ~ log_dens_exp + ..c2 + i(timetotreat, treat, c(-1, -Inf)) | id + ..fe,
-  data = dfm, vcov = ~ ~ id + country + year + country^year,  panel.id = c('id', 'year')
-)
-
 est_sa20 = feols(
   log_exp_w ~  log_dens_exp + ..c2 + sunab(yeartreat, year, -1) | id + ..fe,
-  data = df_countries, vcov = cluster ~ id + country + year + country^year,  panel.id = c('id', 'year')
+  data = df_countries, cluster = ~ id + country + year + country^year,  panel.id = c('id', 'year')
 )
 
-iplot(list("SunAb"=est_sa20, "TWFE"=est_twfe),
-        xlab="Time to Urban Access Regulation",
+ggiplot(est_sa20,
+      xlab="Time to Low Emission Zone",
         main= "",
         geom_style = 'ribbon',
         ref.line = -1,
         multi_style = 'facet'
         ) +
-  scale_x_continuous(breaks=seq(-6, 6), limits = c(-6, 6))+
+  scale_x_continuous(breaks=seq(-5, 5, 1), limits = c(-5, 5))+
   theme(text = element_text(family = "Palatino")) +
   scale_fill_brewer(palette = 'Set1', aesthetics = c("colour", "fill"))
 
-ggsave("es1.png", dpi=300, width=5, height=5)
-
-est_twfe = feols(
-  exposure ~ sum_pop + ..c2 + i(timetotreat, treat, c(-1, -Inf)) | id + ..fe,
-  data = dfm, vcov = ~ id + country + year + country^year,  panel.id = c('id', 'year')
-)
+ggsave("figures/es1.png", dpi=300, width=5, height=5)
 
 est_sa20 = feols(
-  exposure ~ sum_pop + ..c2 + sunab(yeartreat, year, -1) | id + ..fe,
-  data = df_countries, vcov = cluster ~ id + country + year + country^year,  panel.id = c('id', 'year')
+  exposure ~  log_dens_exp + ..c2 + sunab(yeartreat, year, -1) | id + ..fe,
+  data = df_countries, cluster = ~ id + country + year + country^year,  panel.id = c('id', 'year')
 )
 
-iplot(list("SunAb"=est_sa20, "TWFE"=est_twfe),
-        xlab="Time to Urban Access Regulation",
+ggiplot(est_sa20,
+        xlab="Time to Low Emission Zone",
         main= "",
         geom_style = 'ribbon',
         ref.line = -1,
         multi_style = 'facet'
 ) +
-  scale_x_continuous(breaks=seq(-6, 6), limits = c(-6, 6))+
+  scale_x_continuous(breaks=seq(-5, 5), limits = c(-5, 5))+
   theme(text = element_text(family = "Palatino")) +
   scale_fill_brewer(palette = 'Set1', aesthetics = c("colour", "fill"))
 
-ggsave("es2.png", dpi=300, width=5, height=5)
-
-est_twfe = feols(
-  exposure ~ sum_pop + ..c2 + i(timetotreat, treat, c(-1, -Inf)) | id + year,
-  data = dfm, vcov = ~ id + year,  panel.id = c('id', 'year'), split=~country
-)
-
-est_sa20 = feols(
-  exposure ~ sum_pop + ..c2 + sunab(yeartreat, year, -1) | id + ..fe,
-  data = df_countries, vcov = cluster ~ id + year + country^year,  panel.id = c('id', 'year'),
-  split= ~country
-)
-
-ggiplot(list("did"=est_sa20),
-        xlab="Time to Urban Access Regulation",
-        main= "",
-        geom_style = 'ribbon',
-        ref.line = -1,
-        facet_args = list(ncol = 4, scales='free_y', labeller = labeller(id = \(x) gsub(".*: ", "", x))),
-        multi_style = 'facet'
-) +
-  scale_x_continuous(breaks=seq(-3, 3), limits = c(-3, 3))+
-  theme_minimal()+
-  theme(text = element_text(family = "Palatino")) +
-  scale_fill_brewer(palette = 'Set1', aesthetics = c("colour", "fill"))
+ggsave("figures/es2.png", dpi=300, width=5, height=5)
